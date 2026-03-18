bddfa663b11dd6c03c2bd837a667f05231405543
Caden Gardner <caden.gardner1234@gmail.com>
2026-03-18 01:12:49 -0500

    Add files via upload

---
+++ defs.h
@@ -0,0 +1,17 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+// Centralized list of paramters
+
+#define HITBOXES_ON 0 // 1 for hitboxes visible, 0 for not visible
+
+const int SCENE_WIDTH   = 1152; // These should be the dim of the background img
+const int SCENE_HEIGHT  = 648;
+
+const int PLAYER_WIDTH  = 80;
+const int PLAYER_HEIGHT = 80;
+
+const int CROSSHAIR_WIDTH  = 40;
+const int CROSSHAIR_HEIGHT = 40;
+
+#endif
